ca0791fd0efebfabaec3aac8b9820438ba0290a9
Hesham Al Nukhailan <halnukhailan@gmail.com>
2019-10-05 20:19:30 +0300

    WIKI task

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+# from pages import views
 
 # Create your models here.
+
+class Page(models.Model):
+	title = models.CharField(max_length=105)
+	content = models.CharField(max_length=105)
+	last_update = models.DateField(auto_now_add=True)
+
+	def get_absolute_url(self):
+		return "detail/%s" % self.id
+
+	def __str__(self):
+		return self.title
